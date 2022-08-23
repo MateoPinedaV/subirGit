d215f7abd244d586f32e15268f35ece1f2ebf434
mapvv2003 <mapvv2003@gmail.com>
2022-08-23 15:31:06 -0500

    mi primer commit

---
+++ src/main/java/com/mateo/tareaCapacitacion/TareaCapacitacionApplication.java
@@ -0,0 +1,13 @@
+package com.mateo.tareaCapacitacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaCapacitacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaCapacitacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateo/tareaCapacitacion/TareaCapacitacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateo.tareaCapacitacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaCapacitacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
